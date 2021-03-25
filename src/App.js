973bdc3fb35ff01be81f127e4ccba0b89abe45dc
GyuriLee <jae04099@gmail.com>
2021-03-25 22:25:57 +0900

    App.js에 react router 관련 import 제거

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import {BrowserRouter, Route} from 'react-router-dom'
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Card from "./components/Card";
